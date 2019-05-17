ed3f1662ed8d1aec421dedbbb6c1a588cf2bae9e
johniblake <johniblake93@gmail.com>
2019-05-17 18:37:25 -0500

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
 # liri-node-app
+
 A Command Line Interface for searching Spotify, Bands in Town, and OMDB for information about your favorite content and bands.
+
+**How to use Liri**
+
+Liri is a node application, which means each command given to Liri should take the form:
+
+> `node liri.js <command> [query]`
+>
+> _Note: commands should be issued from inside the liri-node-app directory in your terminal_
+
+The **commands** are:
+
+> - concert-this
+> - spotify-this-song
+> - movie-this
+> - do-what-it-says
+
+**Queries** are optional but recommended and can be strings of any length containing the band name, movie title, or track name that you want information about.
+
+_Example:_
+
+> `node liri.js movie-this National Treasure`
+
+_Output:_
+
+> National Treasure
+> 2004
+>
+> IMDB Rating: 6.9
+>
+> Rotten Tomatoes Rating: 45%
+>
+> USA
+>
+> English, Spanish
+>
+> A historian races to find the legendary Templar Treasure before a team of mercenaries.
+>
+> Nicolas Cage, Diane Kruger, Justin Bartha, Sean Bean
+
+_Example:_
+
+> `node liri.js concert-this Nickelback`
+
+_Output:_
+
+> Upcoming Shows for Nickelback:
+
+> Summer Sounds FM Concert Series
+>
+> Fort Mcmurray, Canada
+>
+> 07/06/2019
+
+> Edenvale Aerodrome
+>
+> Stayner, Canada
+>
+> 07/11/2019
+
+> Bank of New Hampshire Pavilion
+>
+> Gilford, NH, United States
+>
+> 07/17/2019
+
+> Parc Beauséjour
+>
+> Rimouski, Canada
+>
+> 07/20/2019
+
+> Mississippi Valley Fair
+>
+> Davenport, IA, United States
+>
+> 08/03/2019
+
+_Example_
+
+> `node liri.js spotify-this-song My Heart Will Go On`
+
+_Output_
+
+> Céline Dion
+>
+> My Heart Will Go On - Love Theme from "Titanic"
+>
+> Preview: https://p.scdn.co/mp3-preview/5fcdcfe7ef20abd006bba666b4a7dff01dd5ec21?cid=ee9cc32007474320b50a760b96e9ce17
+>
+> Let's Talk About Love
+
+_Example_
+
+> `node liri.js do-what-it-says`
+
+_Note:_
+
+> This command takes no arguments will execute whatever command is specified in `liri-node-app/random.txt`
--- liri.js
+++ liri.js
@@ -1,30 +1,24 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-var Spotify = require("node-spotify-api");
+let keys = require("./keys.js");
+let Spotify = require("node-spotify-api");
 let fs = require("fs");
 let axios = require("axios");
-var spotify = new Spotify(keys.spotify);
+let moment = require("moment");
+
+let spotify = new Spotify(keys.spotify);
 
 let command = process.argv[2];
-var query = process.argv.slice(3).join(" ");
-
-// concert-this
-
-// spotify-this-song
-
-// movie-this
-
-// do-what-it-says
+let query = process.argv.slice(3).join(" ");
 
 doThis(command, query);
 
 function doThis(command, query) {
+  log(command, query);
   switch (command) {
     case "concert-this":
       concertThis(query);
       break;
     case "spotify-this-song":
-      console.log("here");
       spotifyThis(query);
       break;
     case "movie-this":
@@ -37,13 +31,18 @@ function doThis(command, query) {
 }
 
 function spotifyThis(query) {
+  if (!query) {
+    console.log("");
+    console.log("No song specified, displaying The Sign by Ace of Base.");
+    query = "The Sign Ace of Base";
+  }
+
   spotify
     .search({ type: "track", query: query, limit: 1 })
     .then(function(response) {
       let tracks = response.tracks.items;
       tracks.forEach(function(track) {
-        console.log(track.name);
-        console.log(track.album.name);
+        displayTrack(track);
       });
     })
     .catch(function(err) {
@@ -52,6 +51,11 @@ function spotifyThis(query) {
 }
 
 function concertThis(artist) {
+  if (!artist) {
+    console.log("");
+    console.log("No artist specified. Displaying results for Nickelback.");
+    artist = "Nickelback";
+  }
   let queryURL =
     "https://rest.bandsintown.com/artists/" +
     artist +
@@ -60,7 +64,12 @@ function concertThis(artist) {
     .get(queryURL)
     .then(function(response) {
       if (response.status === 200) {
-        console.log(response.data);
+        console.log("");
+        console.log("Upcoming Shows for " + artist + ":");
+        console.log("");
+        response.data.forEach(function(show) {
+          displayShow(show);
+        });
       }
     })
     .catch(function(error) {
@@ -69,14 +78,21 @@ function concertThis(artist) {
 }
 
 function movieThis(movie) {
-  var queryUrl =
+  if (!movie) {
+    movie = "Mr. Nobody";
+    console.log("");
+    console.log("No movie was entered. Displaying Results for 'Mr. Nobody'.");
+  }
+  let queryUrl =
     "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
   axios
     .get(queryUrl)
     .then(function(response) {
-      if (response.status === 200) {
-        console.log("The movie's release year is: " + response.data.Year);
+      if (response.status === 200 && response.data.Title) {
+        displayMovie(response);
+      } else {
+        console.log("IMDB does not have information about " + movie);
       }
     })
     .catch(function(error) {
@@ -91,14 +107,74 @@ function doWhatItSays() {
       return console.log(error);
     }
 
-    // We will then print the contents of data
+    let dataArr = data.split(",");
+    let command = dataArr[0];
+    let query = dataArr[1];
 
-    // Then split it by commas (to make it more readable)
-    var dataArr = data.split(",");
-    var command = dataArr[0];
-    var query = dataArr[1];
-
-    // We will then re-display the content as an array for later use.
     doThis(command, query);
   });
+}
+
+function log(command, query) {
+  fs.appendFile("log.txt", command + "," + query + "\n", function(err) {
+    // If an error was experienced we will log it.
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+function getVenueLocation(show) {
+  let place = [];
+  if (show.venue.city) {
+    place.push(show.venue.city);
+  }
+  if (show.venue.region) {
+    place.push(show.venue.region);
+  }
+  if (show.venue.country) {
+    place.push(show.venue.country);
+  }
+  return place.join(", ");
+}
+
+function displayShow(show) {
+  let placeString = getVenueLocation(show);
+  console.log("");
+  console.log(show.venue.name);
+  console.log(placeString);
+  console.log(moment(show.datetime).format("MM/DD/YYYY"));
+  console.log("");
+}
+
+function displayTrack(track) {
+  let artists = [];
+  track.artists.forEach(function(artist) {
+    artists.push(artist.name);
+  });
+  console.log("");
+  console.log(artists.join(", "));
+  console.log("");
+  console.log(track.name);
+  console.log("");
+  console.log("Preview: " + track.preview_url);
+  console.log("");
+  console.log(track.album.name);
+  console.log("");
+}
+
+function displayMovie(response) {
+  console.log("");
+  console.log(response.data.Title);
+  console.log(response.data.Year);
+  console.log("");
+  console.log("IMDB Rating: " + response.data.imdbRating);
+  console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+  console.log(response.data.Country);
+  console.log(response.data.Language);
+  console.log("");
+  console.log(response.data.Plot);
+  console.log("");
+  console.log(response.data.Actors);
+  console.log("");
 }
